b9cd1fa28c14205b17b0e2073a089ba8c9fbd488
Mario <hanlc109@hotmail.com>
2020-07-27 07:44:31 -0700

    add the instance ID receiver

---
+++ FCMClient.Android/MyFirebaseIIDService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Android.App;
+using Firebase.Iid;
+using Android.Util;
+
+namespace FCMClient.Droid
+{
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.INSTANCE_ID_EVENT"})]
+    public class MyFirebaseIIDService : FirebaseInstanceIdService
+    {
+        const string TAG = "MyFirebaseIIDService";
+        public override void OnTokenRefresh()
+        {
+            var refreshToken = FirebaseInstanceId.Instance.Token;
+            Log.Debug(TAG, "Refreshed token: " + refreshToken);
+            SendRegistrationToServer(refreshToken);
+        }
+
+        void SendRegistrationToServer(string token)
+        {
+
+        }
+    }
+}
